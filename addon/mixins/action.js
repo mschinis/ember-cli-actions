04e294d7ac7f0be0693fcc072933bf2609ef76ff
Albert-Jan Nijburg <albertjan@curit.com>
2016-02-25 14:15:24 +0000

    fix comments by @mschinis and add manual implementation in dummy app

--- addon/mixins/action.js
+++ addon/mixins/action.js
@@ -7,8 +7,8 @@ export default Ember.Mixin.create({
         this._super();
         const constructorName = this.constructor.toString();
         const [ , route, name ] = regex.exec(constructorName);
-        if (route !== "route") {
-            throw new Error("The ActionsMixin is currently only applicable to routes");
+        if (route !== 'route') {
+            throw new Error('`ember-cli-actions/mixins/action` is currently only applicable to routes');
         }
 
         const config = this.container.lookupFactory('config:environment');
